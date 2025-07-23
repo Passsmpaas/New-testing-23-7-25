cd5e4bd818ea8dc5afd5a22d04aa0739f226a56b
Strangeboy859152 <166413305+Strangeboy859152@users.noreply.github.com>
2025-07-23 20:52:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1322,7 +1322,7 @@ async def txt_handler(bot: Client, m: Message):
                     remaining_links = len(links) - count
                     progress = (count / len(links)) * 100
                     Show1 =f"╠════════════════════════════════════╣\n"
-                           f"║   👨‍💻 STRANGER BOT DOWNLOADER v1.0      ║\n"
+                           f"║ 👨‍💻 STRANGER BOT DOWNLOADER v1.0 ║\n"
                            f"╚════════════════════════════════════╝\n"
                            f"<blockquote>🚀𝐏𝐫𝐨𝐠𝐫𝐞𝐬𝐬 » {progress:.2f}%</blockquote>\n┃\n" \
                            f"┣🔗𝐈𝐧𝐝𝐞𝐱 » {count}/{len(links)}\n┃\n" \
